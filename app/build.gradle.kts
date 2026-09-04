04e5ac56bbbb628ecdf39938d338b38441838adb
mipanin374-collab <mipanin374@gmail.com>
2026-09-04 15:53:07 +0500

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "ru.hmao.pomoshchnik"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ru.hmao.pomoshchnik"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
