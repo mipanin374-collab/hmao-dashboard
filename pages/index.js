ac5845fcf098189d750dead896ed128639e6fbe0
mipanin374-collab <mipanin374@gmail.com>
2026-09-08 11:53:22 +0500

    Add HMAO portal entry page

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home(){
+  return <iframe title="Помощник ХМАО" src="/portal.html" style={{position:'fixed',inset:0,width:'100%',height:'100%',border:0,background:'#f6f8fc'}} />
+}
